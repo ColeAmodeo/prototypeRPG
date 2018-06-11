2b4c7fca6a0ce9ec96df1e64447072dfa9922b42
ColeAmodeo <camodeo1@live.ca>
2018-06-11 16:09:29 -0400

    jquery linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,10 @@ var enemies =  ["goblin", "lizard", "werewolf", "necromancer", "ghost", "shrek"]
 
 // Hero Selection
 
+$('#mage-test').click(function(){
+    
+    $(".player-portrait").css("background-image", "url('assets/images/mage.jpg')")
+
+}
+
+)
