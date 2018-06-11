4b8ad45dff974c2490d90a981eab1c155cc61e3b
ColeAmodeo <camodeo1@live.ca>
2018-06-11 14:41:16 -0400

    Template Finished

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var magePick 
+var vampirePick 
+var warriorPick 
+var enemies =  ["goblin", "lizard", "werewolf", "necromancer", "ghost", "shrek"];  
+
+
+// Hero Selection
+
+
+$("#mage-test").click(function() {
+
+            $(".player-portrait").addClass('mage')
+    
+})
